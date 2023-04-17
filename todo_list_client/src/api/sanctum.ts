2c940aa8c8bc5c77d7ef612c511178c4c507d5b3
zaytc145 <zaytsevdanil@yandex.ru>
2023-04-17 23:31:19 +0300

    client config

--- todo_list_client/src/api/sanctum.ts
+++ todo_list_client/src/api/sanctum.ts
@@ -2,6 +2,6 @@ import todoApi from "./todoApi";
 
 export const fetchCsrfToken = async () => {
   await todoApi.get("/sanctum/csrf-cookie", {
-    baseURL: "http://localhost:8000",
+    baseURL: import.meta.env.TODO_API_BASE_URL,
   });
 };
